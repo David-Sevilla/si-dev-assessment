faf34d9e52c9f29370661ed1a4263b7f570ea923
DavidSevilla <david@davidsevilla.net>
2017-06-29 23:26:53 +0000

    Finishing Task 6

--- Gemfile
+++ Gemfile
@@ -31,6 +31,7 @@ gem 'bcrypt', '~> 3.1.7'
 # gem 'capistrano-rails', group: :development
 
 gem 'twilio-ruby', '~> 4.11', '>= 4.11.1'
+gem 'figaro'
 
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,7 +3,18 @@ class PagesController < ApplicationController
   end
 
   def sms
-    params[:message]
-    params[:user][:phone]
+    require 'twilio-ruby'
+    
+    account_sid = ENV['TWILIO_ACCOUNT_ID'] # Your Account SID from www.twilio.com/console
+    auth_token = ENV["TWILIO_AUTH_TOKEN"]   # Your Auth Token from www.twilio.com/console
+                  
+    @client = Twilio::REST::Client.new account_sid, auth_token
+    message = @client.account.messages.create(:body => params[:message],
+    :to => params[:user][:phone],    # Replace with your phone number
+    :from => ENV['TWILIO_FROM_NUMBER'])  # Replace with your Twilio number
+    puts message.sid
+    
+    
+    
   end
 end
